3159e8ec57d68ced03344d980290cf01bd77e419
juanguillermo3 <135280741+juanguillermo3@users.noreply.github.com>
2025-03-19 18:32:53 -0500

    Update sample_balance_in_workers_sample.R

--- sample_balance_in_workers_sample.R
+++ sample_balance_in_workers_sample.R
@@ -1,6 +1,6 @@
 #'
 #' Title: sample balance in workers sample 
-#' Description: merges demographic and professional information from workers, along with participation in job intermerdiation, to a single sample. It asses balance in program assignation.
+#' Description: merges demographic and professional information from workers, along with participation in job programs, to a single sample. It asses balance in program assignation.
 #' image_url=assets/*balance.png
 #'
 
